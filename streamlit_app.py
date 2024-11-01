f599bd2c9a53396d03d59518a6794cf0f4938d70
Unni Krishnan <ambady1960@hotmail.com>
2024-11-02 00:18:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from enquiry import enquire
 
 # Show title and description.
 st.title("💬 BCA Project ..🎈")
-st.write( "This is a Agentic Help Tool for Submissins to BCA..")
+st.write( "This is aa Agentic Help Tool for Submissions to BCA.")
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
@@ -18,14 +18,14 @@ options = st.radio("Navigate", ["About", "Enquire", "Require", "Agent", "Ack"], 
 
 # Define functions for "About", "Require", and "Agent"
 def about():
-    st.write("About BCA Project")
+    st.write("**About BCA Project**")
 
 def require():
-    st.write("Requirements Section")
+    st.write("**Requirements Section**")
     st.write("This part allows BCA officers to drag and drop the latest revision in submission requirements.")
     st.write("The enquiry page will enhance its response based on the new set of information provided.")
 def agent():
-    st.write("Agents for Submissions Checking...")
+    st.write("**Agents** for Submissions Checking...")
     st.write("Companies can drag & drop their submissions here.")
     st.write("Integrated AI agents will perform the checking...")
     st.write("A report on compliance and deviations will be generated...")
