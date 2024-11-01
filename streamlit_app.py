e6c2524bd3ac91053b09d673e58833e127b488cb
Unni Krishnan <ambady1960@hotmail.com>
2024-11-01 21:26:33 +0800

    Update streamlit_app.py
    Read AI from Secret

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,9 @@ st.write( "This is a Agentic Help Tool for Submissins to BCA.")
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+#openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_key = st.secrets["openai"]["secret_key"]
+
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
